2b6d37576b342ab84159526c1f8aecca9cc62944
Smith <Adam.Smith@ec.gc.ca>
2024-10-30 12:25:29 -0400

    adding a tweak to the custom trends script options

--- Custom_estimates_extraction.R
+++ Custom_estimates_extraction.R
@@ -47,3 +47,53 @@ write_excel_csv(i,
                 paste0("temp/custom_output/annual_indices_",outname,".csv"))
 write_excel_csv(ism,
                 paste0("temp/custom_output/smoothed_annual_indices_",outname,".csv"))
+
+
+
+
+
+
+# Regional ----------------------------------------------------------------
+
+
+
+
+reg_sel <- c("NB")
+
+outname <- reg_sel# must supply meaningful file name suffix for output files
+
+
+YYYY <- 2022
+
+library(bbsBayes2)
+library(tidyverse)
+
+
+trends <- readRDS(paste0("Website/All_BBS_Trends_",YYYY,".rds"))
+indices <- readRDS(paste0("Website/All_BBS_Full_Indices_",YYYY,".rds"))
+indices_smooth <- readRDS(paste0("Website/All_BBS_Smoothed_Indices_",YYYY,".rds"))
+
+tr <- NULL
+i <- NULL
+ism <- NULL
+
+for(reg in reg_sel){
+  tr <- trends %>%
+    filter(region == reg) %>%
+    bind_rows(.,tr)
+
+  i <- indices %>%
+    filter(region == reg) %>%
+    bind_rows(.,i)
+  ism <- indices_smooth %>%
+    filter(region == reg) %>%
+    bind_rows(.,ism)
+
+}
+
+write_excel_csv(tr,
+                paste0("temp/custom_output/trends_",outname,".csv"))
+write_excel_csv(i,
+                paste0("temp/custom_output/annual_indices_",outname,".csv"))
+write_excel_csv(ism,
+                paste0("temp/custom_output/smoothed_annual_indices_",outname,".csv"))
--- Custom_trends.R
+++ Custom_trends.R
@@ -1,17 +1,24 @@
 ### Custom trend period
 library(tidyverse)
+#pak::pkg_install("bbsBayes/bbsBayes2@v1.1.1") # necessary to support saved objects from previous version
 library(bbsBayes2)
 
-trend_first_year <- 1978
+trend_first_year <- 2002
 trend_last_year <- 2022
 
+species_list <- read_csv("website/2022 BBS trends for website.csv") %>%
+  filter(geo.area == "ON") %>%
+    select(sp,species) %>%
+  distinct() %>%
+    as.data.frame()
 
-species_list <- readRDS("data/prairie_sp_table.rds")
+#species_list <- readRDS("data/prairie_sp_table.rds")
+
 trends_out <- NULL
 for(j in 1:nrow(species_list)){
 
-  species <- species_list[j,"BirdName"]
-  aou <- as.integer(search_species(species)[1,"aou"])
+  species <- species_list[j,"species"]
+  aou <- species_list[j,"sp"]#as.integer(search_species(species)[1,"aou"])
 
   inds <- readRDS(paste0("indices/Inds_",aou,".rds")) #n_smooth
 
@@ -23,10 +30,27 @@ for(j in 1:nrow(species_list)){
                             prob_decrease = c(0,30,50))
 
   tmp <- trends$trends %>%
-    mutate(BirdName = species)
+    mutate(english = species)
   trends_out <- bind_rows(trends_out,tmp)
 
 }
 
-write_csv(trends_out,paste0("extra_output/prairie_sp_trends",
+
+
+trends_out <- trends_out  %>%
+  mutate(across(where(is.double) & !contains("year") &
+                  !starts_with("n_") & !starts_with("bbs_num"),~signif(.,3)))
+
+
+
+trends_on <- trends_out %>%
+  filter(grepl("CA-ON",region) | region == "ON" | region == "Canada")
+
+# write_csv(trends_out,paste0("extra_output/prairie_sp_trends",
+#                             trend_first_year,"_",trend_last_year,".csv"))
+
+
+write_csv(trends_on,paste0("extra_output/Ontario_trends",
                             trend_first_year,"_",trend_last_year,".csv"))
+
+
